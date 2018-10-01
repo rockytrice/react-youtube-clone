1113be748302737f68ca1f0f08de1b3750e13a14
Roderick Trice <rocky_trice@yahoo.com>
2018-10-01 14:59:29 -0400

    added the api key and .env file to hide the key

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+// api key 
+const API_KEY= `${process.env.REACT_APP_API_KEY}`
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
       </div>
     );
   }
