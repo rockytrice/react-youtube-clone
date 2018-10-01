fcd1270235a65da8d81990198fafd1ca22aeea7b
Roderick Trice <rocky_trice@yahoo.com>
2018-10-01 15:14:37 -0400

    changed searchbar fuctional component to a class based component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import SearchBar from "./Components/Search_bar"
 
 // api key 
 const API_KEY= `${process.env.REACT_APP_API_KEY}`
@@ -9,6 +9,7 @@ class App extends Component {
   render() {
     return (
       <div>
+      <SearchBar />
       </div>
     );
   }
---
+++ src/Components/Search_bar.js
@@ -0,0 +1,18 @@
+import React,{Component} from "react"
+
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <input />
+        ) 
+    }
+    
+};
+
+
+
+
+
+
+export default SearchBar;
