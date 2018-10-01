7e6d11316304c33d6b187398159ac6d68ad82402
Roderick Trice <rocky_trice@yahoo.com>
2018-10-01 16:07:03 -0400

    setting initial state of search api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
 import SearchBar from "./Components/Search_bar"
-
+import YTSearch from "youtube-api-search"
 // api key 
 const API_KEY= `${process.env.REACT_APP_API_KEY}`
 
+
+
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={videos:[]};
+    YTSearch({key:API_KEY, term:"surfboards"}, function(data) {
+      console.log(data);
+    });
+  }
   render() {
     return (
       <div>
--- src/Components/Search_bar.js
+++ src/Components/Search_bar.js
@@ -2,13 +2,25 @@ import React,{Component} from "react"
 
 
 class SearchBar extends Component {
-
+    constructor(props){
+        super(props);
+        this.state ={
+            term: ''
+        };
+    }
     render() {
         return (
             // create input element and pass as a prop on change with a value of this.handleInputChange
-            <input onChange={event=> console.log(event.target.value)}
+            <div>
+            <input 
+            value={this.state.term}
+            onChange={event=> 
+            // set the state with the new value of term and pushes all the update info to the dom
+            this.setState(
+                {term:event.target.value})}
              />
-        ) 
+             </div>
+        );
     }
     
 };
