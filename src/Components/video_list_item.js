f16ea05a4df253f273a859019994d3f3adc17088
Roderick Trice <rocky_trice@yahoo.com>
2018-10-03 19:26:05 -0400

    video snippet and title are now rendered to the page

--- src/Components/video_list_item.js
+++ src/Components/video_list_item.js
@@ -1,8 +1,22 @@
 import React from "react";
 
-const VideoListItem = (props) =>{
-    return <li>Video</li>
-}
+const VideoListItem = ({video}) =>{
+    const imageUrl=video.snippet.thumbnails.default.url;
+
+
+    return(
+     <li className="list-group-item" style={{listStyle:"none"}} >
+        <div className="video-list media">
+            <div className="media-left">
+                <img className="media-object" src={imageUrl} alt="img" />
+            </div>
+            <div className="media-body">
+                <div className="media-heading">{video.snippet.title}</div>
+            </div>
+        </div>
+     </li>
+    ); 
+};
 
 
 
