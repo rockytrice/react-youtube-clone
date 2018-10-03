9edb41951415f51904d93b4ddcaca00d1a477ed4
Roderick Trice <rocky_trice@yahoo.com>
2018-10-03 19:06:38 -0400

    successfully added mock video list  on the page

--- src/Components/Video_list.js
+++ src/Components/Video_list.js
@@ -3,9 +3,14 @@ import VideoListItem from "./video_list_item";
 
 
 const VideoList =(props) => {
+//   {/* loop over array and generate one instance of video list item */}
+   const videoItems = props.videos.map((video) => {
+    //    making each video have the same key (etag)
+        return <VideoListItem key= {video.etag} video ={video} />
+    })
     return(
         <ul className="col-md-4 list-group">
-        {/* loop over array and generate one instance of video list item */}
+        {videoItems}
         </ul>
     )
 }
