220bcc718f00d55258f7399f5d7278757e8948dc
Roderick Trice <rocky_trice@yahoo.com>
2018-10-03 20:46:48 -0400

    changed url of  emebed youtube player back to es5. didnt like template strings

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import SearchBar from "./Components/Search_bar"
 import YTSearch from "youtube-api-search"
 import VideoList from "./Components/Video_list"
+import VideoDetail from "./Components/Video_detail";
+
+
+
 // api key 
 const API_KEY= `${process.env.REACT_APP_API_KEY}`
 
@@ -12,7 +16,7 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state={videos:[]};
-    YTSearch({key:API_KEY, term:"surfboards"}, (videos) => {
+    YTSearch({key:API_KEY, term:"nba"}, (videos) => {
       // update the value of videos..this.setState({videos:videos})
       this.setState({videos});
     });
@@ -21,6 +25,7 @@ class App extends Component {
     return (
       <div>
       <SearchBar />
+      <VideoDetail video={this.state.videos[0]} />
       <VideoList videos={this.state.videos} />
       </div>
     );
--- src/Components/Video_detail.js
+++ src/Components/Video_detail.js
@@ -2,13 +2,16 @@ import React from "react";
 
 
 const VideoDetail= ({video})=> {
+    if(!video){
+        return <div>loading..</div>;
+    }
     const videoId = video.id.videoId;
     // embedding video from youtube.. as long as you  have a video id it will embed any video creating an iframe pointing to this url and will show a youtube player in the app
-    const url = `https://www.youtube.com/embed/ ${videoId}`;
+    const url = 'https://www.youtube.com/embed/' + videoId;
 return(
     <div className="video-detail col-md-8">
-        <div className="embed-responsive embed-responsive-16y9">
-            <iframe className="embed-responsive-item" src={url}></iframe>
+        <div className="embed-responsive embed-responsive-16by9">
+            <iframe className="embed-responsive-item" src={url} title="This is a unique title"></iframe>
         </div>
         <div className="details">
             <div>{video.snippet.title}</div>
