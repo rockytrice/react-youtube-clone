b134dd40b57d89a303d3c2cb68df121ad9b442f7
Roderick Trice <rocky_trice@yahoo.com>
2018-10-04 16:03:46 -0400

    minor change to allow fullscreen

--- src/Components/Video_detail.js
+++ src/Components/Video_detail.js
@@ -11,7 +11,7 @@ const VideoDetail= ({video})=> {
 return(
     <div className="video-detail col-md-8">
         <div className="embed-responsive embed-responsive-16by9">
-            <iframe className="embed-responsive-item" src={url} title="This is a unique title"></iframe>
+            <iframe allowfullscreen="0" className="embed-responsive-item" src={url} title="This is a unique title"></iframe>
         </div>
         <div className="details">
             <div>{video.snippet.title}</div>
